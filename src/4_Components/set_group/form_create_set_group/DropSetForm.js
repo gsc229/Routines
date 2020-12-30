df330077de4cb3393add5dcf5dc8f2a5d66f79d3
greg <gsc229@gmail.com>
2020-12-30 11:15:07 -0500

    created reusable inputs and buttons for created setgroups

--- src/4_Components/set_group/form_create_set_group/DropSetForm.js
+++ src/4_Components/set_group/form_create_set_group/DropSetForm.js
@@ -1,31 +1,36 @@
 import React, {useEffect, useState} from 'react'
 import { connect } from 'react-redux'
-import {createSetGroupWithSets} from '../../../3_APIs/setGroupApi'
 import {writingCreateSetGroupData} from '../../../1_Actions/setGroupActions'
 import {Link} from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-import {FiArrowRight} from 'react-icons/fi'
+import {
+  ConnectedRepMaxInput, 
+  ConnectedRestSecondsInput, 
+  ConnectedWeightInput, 
+  ConnectedTotalSetsInput,
+  ConnectedNextStepButton
+} from './shared_btns_and_inputs/SetGroupBtnsAndInputs'
+
+
 
 export const DropSetForm = ({
   writingCreateSetGroupData,
-  createSetGroupData,
-  currentSetGroup
+  createSetGroupData
 }) => {
 
   const handleChange = e => {
     writingCreateSetGroupData(e.target.name, e.target.value)
   }
 
-  const {rep_max, weight, percent_decrease, total_sets, rest_time} = createSetGroupData
+  const {rep_max, weight, percent_decrease, total_sets} = createSetGroupData
 
-  const [allowAddExercixe, setAllowedExercise] = useState(rep_max !== "" && percent_decrease !== "" && total_sets !=="")
+  const [allowAddExercixe, setAllowedExercise] = useState(rep_max !== "" && percent_decrease !== "" && total_sets !=="" && weight !== "")
 
   useEffect(()=>{
-    setAllowedExercise(rep_max !== "" && percent_decrease !== "" && total_sets !=="")
+    setAllowedExercise(rep_max !== "" && percent_decrease !== "" && total_sets !=="" && weight !== "")
   },[createSetGroupData])
 
 
@@ -37,32 +42,15 @@ export const DropSetForm = ({
 
        <Col sm='12' className='input-column'>
         <Link>Rep Max Calculator</Link>
+        
        </Col>
 
        <Col className='input-column' lg='4' sm='12'>
-         <InputGroup>
-         <InputGroup.Prepend>
-            <InputGroup.Text>Base on</InputGroup.Text>
-          </InputGroup.Prepend>
-          <Form.Control onChange={handleChange} value={rep_max} name='rep_max' type='number' min={1}/>
-          <InputGroup.Append>
-            <InputGroup.Text>Rep Max</InputGroup.Text>
-          </InputGroup.Append>
-         </InputGroup>
+         <ConnectedRepMaxInput />
        </Col>
 
        <Col className='input-column' lg='3' sm='12'>
-        <InputGroup>
-        <InputGroup.Prepend>
-          <InputGroup.Text>
-            @
-          </InputGroup.Text>
-        </InputGroup.Prepend>
-        <Form.Control onChange={handleChange} value={weight} name='weight' type='number' min={1}/>
-          <InputGroup.Append>
-            <InputGroup.Text>lbs/kgs</InputGroup.Text>
-          </InputGroup.Append>
-        </InputGroup>
+        <ConnectedWeightInput />
        </Col>
 
        <Col className='input-column' lg='4' sm='12'>
@@ -70,7 +58,12 @@ export const DropSetForm = ({
           <InputGroup.Prepend>
             <InputGroup.Text>Drop</InputGroup.Text>
           </InputGroup.Prepend>
-          <Form.Control onChange={handleChange} value={percent_decrease} name='percent_decrease' min={1} type='number' />
+          <Form.Control 
+          onChange={handleChange} 
+          value={percent_decrease} 
+          name='percent_decrease' 
+          max={99}
+          min={1} type='number' />
           <InputGroup.Append>
             <InputGroup.Text>% each set</InputGroup.Text>
           </InputGroup.Append>
@@ -78,29 +71,18 @@ export const DropSetForm = ({
        </Col>
 
        <Col className='input-column' lg='3' sm='12'>
-         <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Total Sets</InputGroup.Text>
-          </InputGroup.Prepend>
-          <Form.Control onChange={handleChange} value={total_sets} name='total_sets' max={20} min={1} type='number' />
-         </InputGroup>
+         <ConnectedTotalSetsInput />
        </Col>
 
        <Col className='input-column' lg='3' sm='12'>
-         <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Rest Seconds</InputGroup.Text>
-          </InputGroup.Prepend>
-          <Form.Control onChange={handleChange} value={rest_time} name='rest_time' min={0} type='number' />
-         </InputGroup>
+         <ConnectedRestSecondsInput />
        </Col>
 
        <Col className='input-column'  sm='12'>
-        <Button
-        className={`add-exercise-btn ${!allowAddExercixe && 'disabled-allow-exercise-btn'}`}
-        onClick={() => writingCreateSetGroupData('currentStep', 'choose-exercise')} 
-        disabled={!allowAddExercixe}>Proceed to add exercise &nbsp;
-        <FiArrowRight style={{color:'white'}} className='next-link' /></Button>
+        <ConnectedNextStepButton
+         disabled={!allowAddExercixe}
+         text={'Proceed to add exercise'}
+         />
        </Col>
      </Row>
 
---
+++ src/4_Components/set_group/form_create_set_group/shared_btns_and_inputs/SetGroupBtnsAndInputs.js
@@ -0,0 +1,151 @@
+
+import React from 'react'
+import { connect } from 'react-redux'
+import {writingCreateSetGroupData} from '../../../../1_Actions/setGroupActions'
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
+import {FiArrowRight, FiArrowLeft} from 'react-icons/fi'
+
+const RepMaxInput = ({
+  createSetGroupData,
+  writingCreateSetGroupData
+}) => {
+  const {rep_max} = createSetGroupData
+
+  return (
+    <InputGroup>
+      <InputGroup.Prepend>
+        <InputGroup.Text>Base on</InputGroup.Text>
+      </InputGroup.Prepend>
+        <Form.Control 
+        onChange={(e)=> writingCreateSetGroupData(e.target.name, e.target.value)} 
+        value={rep_max} 
+        name='rep_max' 
+        type='number'
+        min={1}/>
+      <InputGroup.Append>
+        <InputGroup.Text>Rep Max</InputGroup.Text>
+      </InputGroup.Append>
+    </InputGroup>
+  )
+}
+
+const WeightInput = ({
+  createSetGroupData,
+  writingCreateSetGroupData
+}) => {
+  const {weight} = createSetGroupData
+
+  return (
+    <InputGroup>
+      <InputGroup.Prepend>
+        <InputGroup.Text>
+          @
+        </InputGroup.Text>
+      </InputGroup.Prepend>
+        <Form.Control 
+        onChange={(e) => writingCreateSetGroupData(e.target.name, e.target.value)} 
+        value={weight} 
+        name='weight' 
+        type='number'
+        min={1}/>
+      <InputGroup.Append>
+        <InputGroup.Text>lbs/kgs</InputGroup.Text>
+      </InputGroup.Append>
+    </InputGroup>
+  )
+}
+
+const TotalSetsInput = ({
+  createSetGroupData,
+  writingCreateSetGroupData
+}) => {
+  const {total_sets} = createSetGroupData
+
+  return (
+    <InputGroup>
+      <InputGroup.Prepend>
+        <InputGroup.Text>Total Sets</InputGroup.Text>
+      </InputGroup.Prepend>
+        <Form.Control 
+        onChange={(e) => writingCreateSetGroupData(e.target.name, e.target.value)} 
+        value={total_sets} name='total_sets' 
+        max={20} 
+        min={1} type='number' />
+    </InputGroup>
+)
+}
+
+const RestSecondsInput = ({
+  createSetGroupData,
+  writingCreateSetGroupData
+}) => {
+  const {rest_time} = createSetGroupData
+
+  return (
+    <InputGroup>
+      <InputGroup.Prepend>
+        <InputGroup.Text>Rest Seconds</InputGroup.Text>
+      </InputGroup.Prepend>
+        <Form.Control 
+        onChange={(e) => writingCreateSetGroupData(e.target.name, e.target.value)}  
+        value={rest_time} 
+        name='rest_time' 
+        min={0} 
+        type='number' />
+    </InputGroup>
+  )
+}
+
+const NextStepButton = ({
+  disabled,
+  text,
+  writingCreateSetGroupData,
+}) => {
+
+  return (
+    <Button
+      className={`step-btn next-step-btn ${disabled && 'disabled-next-step-btn'}`}
+      onClick={() => writingCreateSetGroupData('currentStep', 'choose-exercise')} 
+      disabled={disabled}>
+        {text} &nbsp;
+        <FiArrowRight style={{color:'white'}} className='next-link' />
+    </Button>
+  )
+}
+
+const PreviousStepButton = ({
+  disabled,
+  text,
+  writingCreateSetGroupData,
+}) => {
+
+  return (
+    <Button
+      className={`step-btn previous-step-btn ${disabled && 'disabled-previous-step-btn'}`}
+      onClick={() => writingCreateSetGroupData('currentStep', 'choose-exercise')} 
+      disabled={disabled}>
+        <FiArrowLeft style={{color:'white'}} className='previous-link' />
+        &nbsp;   
+        {text}
+    </Button>
+  )
+}
+
+
+const mapStateToProps = (state) => ({
+  createSetGroupData: state.setGroupReducer.createSetGroupData,
+  currentSetGroup: state.setGroupReducer.currentSetGroup
+})
+
+const mapDispatchToProps = {
+  writingCreateSetGroupData
+}
+
+export const ConnectedRepMaxInput = connect(mapStateToProps, mapDispatchToProps)(RepMaxInput)
+export const ConnectedTotalSetsInput = connect(mapStateToProps, mapDispatchToProps)(TotalSetsInput)
+export const ConnectedWeightInput = connect(mapStateToProps, mapDispatchToProps)(WeightInput)
+export const ConnectedRestSecondsInput = connect(mapStateToProps, mapDispatchToProps)(RestSecondsInput)
+export const ConnectedPreviousStepButton = connect(mapStateToProps, mapDispatchToProps)(PreviousStepButton)
+export const ConnectedNextStepButton = connect(mapStateToProps, mapDispatchToProps)(NextStepButton)
