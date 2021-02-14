7004a2460ed1a00cb6324dda53fcdcd2302a3df7
greg <gsc229@gmail.com>
2021-02-14 17:35:05 -0500

    execute exercsie set autofill

--- src/4_Components/execute_sets/RecordInputs.js
+++ src/4_Components/execute_sets/RecordInputs.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import { connect } from 'react-redux'
-import {saveExerciseSetChanges, setCurrentExerciseSet} from '../../1_Actions/exerciseSetActions'
+import {saveExerciseSetChanges, setCurrentExerciseSet, localWritingExerciseSet} from '../../1_Actions/exerciseSetActions'
 import Button from 'react-bootstrap/Button'
 import NavLink from 'react-bootstrap/NavLink'
 import RecordSetList from './RecordSetList'
 
 export const RecordInputs = ({
   currentExerciseSet,
+  localWritingExerciseSet,
   saveExerciseSetChanges,
   setCurrentExerciseSet,
   userRoutines,
@@ -84,6 +85,12 @@ export const RecordInputs = ({
     return false
   }
 
+ const handleAutoFill = () => {
+   targets.forEach(target => {
+     localWritingExerciseSet(target.field_name.replace('target', 'actual'), target.value)
+   })
+ }
+
   return (
 
     <div
@@ -91,6 +98,15 @@ export const RecordInputs = ({
         
 
           <div className='submit-and-cancel-btns'>
+          <Button
+            size='sm'
+            onClick={handleAutoFill}
+            variant='danger'
+            className='submit-all-btn'>
+              Auto Fill
+            </Button>
+
+
             <Button
             size='sm'
             disabled={!showActualsHaveChanged()}
@@ -132,7 +148,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = {
   saveExerciseSetChanges,
-  setCurrentExerciseSet
+  setCurrentExerciseSet,
+  localWritingExerciseSet
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(RecordInputs)
