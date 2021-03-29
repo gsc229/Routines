f19fceb19479de3d382211bae1b258e87e086529
greg <gsc229@gmail.com>
2021-03-29 15:13:38 -0400

    isDemo bool on landing menue for create account

--- src/4_Components/menus/LandingMenu.js
+++ src/4_Components/menus/LandingMenu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link, useLocation} from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { isDemo } from '../../config/config'
 
 const activeStyle = {
   color: 'rgb(160, 14, 14)'
@@ -22,7 +23,7 @@ const LandingMenu = () => {
         <Navbar.Collapse id="navbarNav">
         <Nav className='ml-auto'>
           <Nav.Link style={isActiveTab('/signin').style} active={isActiveTab('/signin').active} as={Link} to='/signin'>Sign In</Nav.Link>
-          <Nav.Link style={isActiveTab('/signup').style} active={isActiveTab('/signup').active} as={Link} to="/signup">Create Account</Nav.Link>
+          {!isDemo && <Nav.Link style={isActiveTab('/signup').style} active={isActiveTab('/signup').active} as={Link} to="/signup">Create Account</Nav.Link>}
           
         </Nav>
         </Navbar.Collapse>
