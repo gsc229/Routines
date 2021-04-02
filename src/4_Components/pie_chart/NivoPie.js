f6775f0bca4408c29b1eb2baec440b90fe6827db
greg <gsc229@gmail.com>
2021-04-01 20:51:09 -0400

    trying svg again...backend dyno on heroku hadn't woken up. could be why the chart wasn't rendering bc there was no data

--- src/4_Components/pie_chart/NivoPie.js
+++ src/4_Components/pie_chart/NivoPie.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ResponsivePieCanvas} from '@nivo/pie'
+import {ResponsivePie} from '@nivo/pie'
 import randomColor from 'randomcolor'
 import {defs} from './defs'
 import {fill} from './fill'
@@ -29,7 +29,7 @@ const NivoPie = ({exSetStratumData={}, exerciseNameMuscleGroupColor}) => {
   return (
     <div 
     className="novi-pie-chart-container">
-      <ResponsivePieCanvas
+      <ResponsivePie
         data={exerciseData}
         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
         innerRadius={.5}
